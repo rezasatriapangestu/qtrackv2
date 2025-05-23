ffbbe226d69aa33c40d308411473ba3a1acc5107
rezasatria12324 <rezasatriapangestu@gmail.com>
2025-05-24 05:20:18 +0700

    upp

--- application/config/config.php
+++ application/config/config.php
@@ -23,7 +23,7 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 | a PHP script and you can easily do that on your own.
 |
 */
-$config['base_url'] = 'https://bitap.my.id/qtrackv2/';
+$config['base_url'] = ((isset($_SERVER['HTTPS']) && $_SERVER['HTTPS'] === 'on') ? "https" : "http") . "://" . $_SERVER['HTTP_HOST'] . "/qtrackv2/";
 
 /*
 |--------------------------------------------------------------------------
